492c510141e74d0247eda52519b4cdd3d6d98817
Caleb Thornber <cth.rnber@gmail.com>
2026-05-12 14:46:06 +0100

    altering high_gc

--- scripts/high_GC.sh
+++ scripts/high_GC.sh
@@ -6,13 +6,13 @@
 #SBATCH --mem=8g
 #SBATCH --time=1:00:00
 #SBATCH --job-name=high_GC
-#SBATCH --output=/share/hoverflies/logsOut/slurm-%x-%j.out
-#SBATCH --error=/share/hoverflies/logsErr/slurm-%x-%j.err
+#SBATCH --output=/share/hoverflies/Caleb/logsOut/slurm-%x-%j.out
+#SBATCH --error=/share/hoverflies/Caleb/logsErr/slurm-%x-%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=XXX@nottingham.ac.uk
 
 source $HOME/.bash_profile
-conda actovate hoverflies
+conda activate hoverflies
 #Activates conda env
 
 PATH_TO=/share/hoverflies/
@@ -21,7 +21,12 @@ PATH_TO=/share/hoverflies/
 mkdir -p $PATH_TO/Caleb/high_GC
 #Creates output directory
 
-seqkit seq -g -m 65 $PATH_TO/fastqs/VB21036_R1.fastq.gz > $PATH_TO/Caleb/high_GC/VB21036_R1.fastq
-seqkit seq -g -m 65 $PATH_TO/fastqs/VB21036_R2.fastq.gz > $PATH_TO/Caleb/high_GC/VB21036_R2.fastq
+seqkit fx2tab -g $PATH_TO/fastqs/VB21036_R1.fastq.gz | \
+awk '$2 >= 65 && $2 <= 80' | \
+seqkit tab2fx > $PATH_TO/Caleb/high_GC/VB21036_R1.fastq
+
+seqkit fx2tab -g $PATH_TO/fastqs/VB21036_R2.fastq.gz | \
+awk '$2 >= 65 && $2 <= 80' | \
+seqkit tab2fx > $PATH_TO/Caleb/high_GC/VB21036_R2.fastq
 #Runs seqkit on sample identifed with high GC reads. Outputs in unzipped format
-#Filters for samples with GC > 65 (-m 65)
+#Filters for samples with GC > 0.65
