fb2c112d01b52ef4ed1759330c4a5bc2ee67294e
Caleb Thornber <cth.rnber@gmail.com>
2026-05-13 14:32:56 +0100

    Changing memory in gc_filter.sh

--- scripts/gc_filter.sh
+++ scripts/gc_filter.sh
@@ -3,9 +3,9 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=8g
+#SBATCH --mem=64g
 #SBATCH --time=1:00:00
-#SBATCH --job-name=BAM_prep
+#SBATCH --job-name=GC_filter
 #SBATCH --output=/share/hoverflies/Caleb/logsOut/slurm-%x-%j.out
 #SBATCH --error=/share/hoverflies/Caleb/logsErr/slurm-%x-%j.err
 #SBATCH --mail-type=ALL
@@ -21,13 +21,19 @@ mapfile -t NAMES < $PATH_TO/Caleb/high_gc_list.txt
 
 SAMPLE_NAME=${NAMES[$SLURM_ARRAY_TASK_ID]}
 
-SAMPLE=$PATH_TO/$SAMPLE_NAME
+SAMPLE_gz=$PATH_TO/fastqs/$SAMPLE_NAME
 
-SAMPLE_ROOT=$(basename $SAMPLE_NAME .fastq)
+SAMPLE_ROOT=$(basename $SAMPLE_NAME .fastq.gz)
 OUT_NAME=${SAMPLE_ROOT}".filtered.fasta"
+
+gunzip -c $SAMPLE_gz > $PATH_TO/Caleb/high_GC/$SAMPLE_ROOT".fastq"
+
+SAMPLE=$PATH_TO/Caleb/high_GC/$SAMPLE_ROOT".fastq"
 
 echo "$SAMPLE_NAME filtered by GC"
 
 OUT=$PATH_TO/$OUT_NAME
 
-printf "%s" "$SAMPLE" | python gc_filter.py > $OUT
+SCRIPT=/gpfs01/home/mbyct9/GIT/4137_Hoverfly_Project_2526/scripts/gc_filter.py
+
+printf "%s" "$SAMPLE" | python $SCRIPT > $OUT
