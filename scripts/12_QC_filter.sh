61aca826a36ee923bfe681e10a72da9fc354d379
Caleb Thornber <cth.rnber@gmail.com>
2026-05-18 14:03:45 +0100

    Adding plink gwas scripts

--- scripts/12_QC_filter.sh
+++ scripts/12_QC_filter.sh
@@ -16,4 +16,9 @@ conda actovate hoverflies
 
 cd /share/hoverflies/Caleb/plink
 
-plink --bfile dog_raw --allow-extra-chr --geno 0.05 --mind 0.05 --maf 0.001 --make-bed --out dog_qc
+plink --bfile VB_raw --allow-extra-chr \
+	--geno 0.05 \
+	--mind 0.05 \
+	--maf 0.001 \
+	--make-bed \
+	--out VB_qc
---
+++ scripts/13_GWAS1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16g
+#SBATCH --time=2:00:00
+#SBATCH --job-name=GWAS1
+#SBATCH --output=/share/hoverflies/Caleb/logsOut/slurm-%x-%j.out
+#SBATCH --error=/share/hoverflies/Caleb/logsErr/slurm-%x-%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=XXX@nottingham.ac.uk
+
+module load plink-uoneasy/1.9b_6.21-x86_64
+
+PATH_TO=/share/hoverflies/Caleb
+
+cd $PATH_TO/plink
+
+plink --bfile VB_qc \
+        --allow-extra-chr \
+        --allow-no-sex \
+        --pheno pheno.txt \
+        --linear \
+        --out GWAS1
