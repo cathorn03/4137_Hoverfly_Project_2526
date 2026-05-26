9965a36cbd8278f3c80412b7983eff7063f560f4
Caleb Thornber <cth.rnber@gmail.com>
2026-05-26 15:11:03 +0100

    Fixing slurm header

--- scripts/10_Annotation_prep.sh
+++ scripts/10_Annotation_prep.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64g
 #SBATCH --time=1:00:00
-#SBATCH --job-name=Annotation prep
+#SBATCH --job-name=Annotation_prep
 #SBATCH --output=/share/hoverflies/Caleb/logsOut/slurm-%x-%j.out
 #SBATCH --error=/share/hoverflies/Caleb/logsErr/slurm-%x-%j.err
 #SBATCH --mail-type=ALL
@@ -22,6 +22,6 @@ REF=$PATH_TO/GCA_949129095.1_idVolBomb1.1_genomic.gbff
 
 OUT=$PATH_TO/VolBomb1.1.gff3
 
-SCRIPT=/gpfs01/home/mbyct9/GIT/4137_Hoverfly_Project_2526/scripts/gbff_to_gff3.py
+SCRIPT=/gpfs01/home/mbyct9/GIT/4137_Hoverfly_Project_2526/scripts/gbff_convert.py
 
 printf "%s" "$SAMPLE" | python $SCRIPT > $OUT
