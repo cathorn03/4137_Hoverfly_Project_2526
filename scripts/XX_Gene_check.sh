2bfcb2a0dc44e2e08acd6cf822819304517777c8
Caleb Thornber <cth.rnber@gmail.com>
2026-05-27 10:50:04 +0100

    Changing out file

--- scripts/XX_Gene_check.sh
+++ scripts/XX_Gene_check.sh
@@ -25,7 +25,7 @@ GENES_BED=$PATH_TO/genes/genes.bed
 grep -v "^#" $REF| awk '$3=="gene"' | cut -f1,4,5,9 > $GENES_BED
 
 FST_BED=$PATH_TO/genes/high_fst.bed
-OUT=$PATH_TO/genes/candidate_genes.gff3
+OUT=$PATH_TO/genes/genes_in_windows.txt
 
 bedtools intersect \
 -a $FST_BED \
