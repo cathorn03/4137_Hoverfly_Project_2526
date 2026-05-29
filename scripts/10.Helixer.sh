edc7a3df3455337d5ca175c2b0bfb54a4cba4e44
Caleb Thornber <cth.rnber@gmail.com>
2026-05-29 13:40:03 +0100

    Creating helixer file

--- scripts/10.Helixer.sh
+++ scripts/10.Helixer.sh
@@ -5,19 +5,23 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=8g
 #SBATCH --time=4:00:00
-#SBATCH --job-name=MAKER
+#SBATCH --job-name=Helixer
 #SBATCH --output=/share/hoverflies/Caleb/logsOut/slurm-%x-%j.out
 #SBATCH --error=/share/hoverflies/Caleb/logsErr/slurm-%x-%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=XXX@nottingham.ac.uk
 
 source $HOME/.bash_profile
-conda activate eukan
+conda activate Helixer
 
-PATH_TO=/share/hoverflies/Caleb/
+PATH_TO=/share/hoverflies/Caleb
 
-GEN=$PATH_TO/references/GCA_949129095.1_idVolBomb1.1_genomic.fasta
-OUT=$PATH_TO/VolBomb1.1.gff3
+Helixer=$PATH_TO/Helixer/Helixer.py
+FASTA=$PATH_TO/references/GCA_949129095.1_idVolBomb1.1_genomic.fasta
+OUT=$PATH_TO/references/helixer_VolBomb1.1.gff3
 
-eukan annotate -g $GEN -k animal -n 8 > $OUT
-
+python $Helixer \
+	--lineage invertebrate \
+	--fasta-path $FASTA \
+	--species Volucella_bombylans \
+	--gff-output-path $OUT
