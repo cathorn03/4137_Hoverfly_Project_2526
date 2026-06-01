9f2aeb8bf1713007b00d35fc33df9898a6f9cbb1
Caleb Thornber <cth.rnber@gmail.com>
2026-06-01 17:18:07 +0100

    correcting paths

--- scripts/10_Gene_check.sh
+++ scripts/10_Gene_check.sh
@@ -20,12 +20,12 @@ module load bedtools-uoneasy/2.31.0-GCC-12.3.0
 PATH_TO=/share/hoverflies/Caleb
 
 REF=$PATH_TO/references/GCA_949129095.1_idVolBomb1.1_genomic.gff
-GENES_BED=$PATH_TO/genes/genes.bed
+GENES_BED=$PATH_TO/haplotype_1/genes/genes.bed
 
 grep -v "^#" $REF| awk '$3=="gene"' > $GENES_BED
 
-FST_BED=$PATH_TO/genes/high_fst.bed
-OUT=$PATH_TO/genes/genes_in_windows.txt
+FST_BED=$PATH_TO/haplotype_1/genes/high_fst.bed
+OUT=$PATH_TO/haplotype_1/genes/genes_in_windows.txt
 
 bedtools intersect \
 -a $FST_BED \
