02bede791655f979ba9d9dfe91d41def45efa918
Caleb Thornber <cth.rnber@gmail.com>
2026-06-03 08:34:40 +0100

    Changing get_chrs to alternate haplotype

--- scripts/get_chrs.sh
+++ scripts/get_chrs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE=/share/hoverflies/Caleb/references/GCA_949129095.1_idVolBomb1.1_genomic.fna 
-OUT=/share/hoverflies/Caleb/chr_list.txt
+FILE=/share/hoverflies/Caleb/references/GCA_949129105.1_idVolBomb1.1_alternate_haplotype_genomic.fasta
+OUT=/share/hoverflies/Caleb/alt_chr_list.txt
 
 grep "^>" $FILE | grep -o '>[^ ]*' | sed 's/^>//' > $OUT
