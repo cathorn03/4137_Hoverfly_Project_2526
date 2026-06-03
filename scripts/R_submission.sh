c7ad704ce26afda15c9dc00f5966b64a5431af1c
Caleb Thornber <cth.rnber@gmail.com>
2026-06-03 10:27:46 +0100

    Increasing mem

--- scripts/R_submission.sh
+++ scripts/R_submission.sh
@@ -3,8 +3,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=12g
-#SBATCH --time=48:00:00
+#SBATCH --mem=64g
+#SBATCH --time=1:00:00
 #SBATCH --job-name=Manhattan_plot
 #SBATCH --output=/share/hoverflies/Caleb/logsOut/slurm-%x-%j.out
 #SBATCH --error=/share/hoverflies/Caleb/logsErr/slurm-%x-%j.err
