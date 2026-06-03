ad07bc652391413becac039c37fbc137da77cdaf
Caleb Thornber <cth.rnber@gmail.com>
2026-06-03 10:32:36 +0100

    Chnaging chrom file

--- scripts/get_chrs.sh
+++ scripts/get_chrs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE=/share/hoverflies/Caleb/references/GCA_949129105.1_idVolBomb1.1_alternate_haplotype_genomic.fasta
+FILE=/share/hoverflies/Caleb/ragtag/ragtag.scaffold.fasta
 OUT=/share/hoverflies/Caleb/alt_chr_list.txt
 
 grep "^>" $FILE | grep -o '>[^ ]*' > $OUT
