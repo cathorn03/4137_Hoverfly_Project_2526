d6b8df1c2c2d9b7c8f9e050faae22becb4052e2c
Caleb Thornber <cth.rnber@gmail.com>
2026-06-08 22:29:59 +0100

    Changing gff remapping method

--- scripts/gff_rename.sh
+++ scripts/gff_rename.sh
@@ -6,4 +6,4 @@ awk 'BEGIN{FS=OFS="\t"}
 FNR==NR {map[$1]=$2; next}
 (/^#/){print; next}
 {$1 = ($1 in map ? map[$1] : $1); print}
-' GCA_949129095.1_idVolBomb1.1_genomic.gff > test.gff
+' chrom_map.txt GCA_949129095.1_idVolBomb1.1_genomic.gff > test.gff
