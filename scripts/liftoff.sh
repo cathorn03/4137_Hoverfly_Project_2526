cf7d9f8c53f9d4896ce1cc1541b9af62a40de945
Caleb Thornber <cth.rnber@gmail.com>
2026-06-09 19:27:59 +0100

    Adding args to liftoff

--- scripts/liftoff.sh
+++ scripts/liftoff.sh
@@ -14,14 +14,54 @@
 source $HOME/.bash_profile
 conda activate hoverflies
 
-PATH_TO=/share/hoverflies/Caleb/references
-TARGET_ROOT=idVolBomb13_plumata
-REF_ROOT=GCA_949129095.1_idVolBomb1.1_genomic
+usage(){
+  echo "Usage: $0 [options]"
+  echo
+  echo "Options:"
+  echo "  -t, --target      Target reference fasta"
+  echo "  -f, --refernce    reference fasta file gff file"
+  echo "  -g, --gff         gff annotation file to lift genes from"
+  echo "  -o, --out         Output file for the liftoff gff"
+  echo "  -h, --help        Show this help message"
+}
 
-TARGET=$PATH_TO/$TARGET_ROOT.fa
-REF=$PATH_TO/$REF_ROOT.fasta
-GFF=$PATH_TO/renamed_$REF_ROOT.gff
-OUT=$PATH_TO/$TARGET_ROOT.gff
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -t|--target)
+      [[ -z "$2" || "$2" == -* ]] && { echo "Missing argument for $1"; exit 1; }
+      TARGET="$2"
+      shift 2 ;;
+
+    -f|--reference)
+      [[ -z "$2" || "$2" == -* ]] && { echo "Missing argument for $1"; exit 1; }
+      REF="$2" 
+      shift 2 ;;
+
+    -g|--gff)
+      [[ -z "$2" || "$2" == -* ]] && { echo "Missing argument for $1"; exit 1; }
+      GFF="$2" 
+      shift 2 ;;
+
+    -o|--out)
+      [[ -z "$2" || "$2" == -* ]] && { echo "Missing argument for $1"; exit 1; }
+      OUT="$2" 
+      shift 2 ;;
+
+    -|--gff)
+      [[ -z "$2" || "$2" == -* ]] && { echo "Missing argument for $1"; exit 1; }
+      PRE="$2" 
+      shift 2 ;;
+
+    -h|--help)
+      usage
+      exit 0
+      ;;
+
+    *) echo "Invalid option: $1" 
+      exit 1 ;;
+  esac
+done
+
 
 liftoff -g $GFF \
 	-o $OUT \
