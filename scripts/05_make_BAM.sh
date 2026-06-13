3313d6af2dc5d0da8ebb27e2dff1159956d8ea95
Caleb Thornber <cth.rnber@gmail.com>
2026-06-13 12:10:36 +0100

    Tuning Mem

--- scripts/05_make_BAM.sh
+++ scripts/05_make_BAM.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
-#SBATCH --mem=8g
+#SBATCH --mem=32g
 #SBATCH --time=48:00:00
 #SBATCH --job-name=05_make_BAM
 #SBATCH --output=/share/hoverflies/Caleb/logsOut/slurm-%x-%j.out
